846f782ba7300373f8f625ea46c6629afea01a17
Joachim Jasmin <joachim.jasmin@gmail.com>
2023-12-12 14:52:48 +0100

    socket integration with uid

--- sockets/config.js
+++ sockets/config.js
@@ -18,9 +18,8 @@ module.exports = (server) => {
 	});
 
 	io.on("connection", (socket) => {
-    console.log("Client connected", socket.id);
 		socket.on("disconnect", () => {
-			console.log("Client disconnected", socket.id);
+			null;
 		});
 		sockets(io, socket);
 	});
--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,5 +1,8 @@
 const sockets = async (io, socket) => {
-
+	socket.on("connectUser", (data, callback) => {
+		socket.uid = data.uid;
+		callback("User connected");
+	});
 };
 
 module.exports = sockets;
